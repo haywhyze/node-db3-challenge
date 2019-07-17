3911a9a8090444cb54f7479f0b095b25351c0508
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-17 16:02:52 +0100

    feat: implement findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,7 +10,15 @@ function findById(id) {
     .first();
 }
 
+function findSteps(id) {
+  return db("steps")
+    .join("schemes", "schemes.id", "scheme_id")
+    .select("steps.*", "scheme_name as scheme")
+    .where("scheme_id", id);
+}
+
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps,
 }
